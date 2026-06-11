8a94fabd80d23927e39111836f438692468da32a
Denys Dziuba <denys@wnm.be>
2026-06-11 17:42:46 +0200

    chore: wire GroupsPageComponent into app root and add global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { GroupsPageComponent } from './components/groups-page/groups-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [GroupsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'worldcup-2026';
-}
+export class AppComponent {}
